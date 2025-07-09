931da2fec79de6743c11117dfdead50ffc16ef4e
rahu.gorad <rahul.gorad@cmss.in>
2025-07-09 19:10:29 +0530

    chore: id add

--- src/ui/scroll-area.tsx
+++ src/ui/scroll-area.tsx
@@ -12,7 +12,7 @@ const ScrollArea = React.forwardRef<
     className={cn("relative overflow-hidden", className)}
     {...props}
   >
-    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit] block!" asChild>
+    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit] block!" id="custom-element" asChild>
       {children}
     </ScrollAreaPrimitive.Viewport>
     <ScrollBar />
