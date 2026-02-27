a7f1ab279333951231ad5b8cc5dda56f79460222
Sid-1819 <siddhesh.shirdhankr18@gmail.com>
2026-02-27 18:54:05 +0530

    fix: update transition functions in fade variants and correct timer type in RouteLoadingProgress

--- src/components/animate/variants/fade.ts
+++ src/components/animate/variants/fade.ts
@@ -16,8 +16,8 @@ export const varFade = (props?: VariantsType) => {
 		// IN
 		in: {
 			initial: { opacity: 0 },
-			animate: { opacity: 1, transition: varTranEnter },
-			exit: { opacity: 0, transition: varTranExit },
+			animate: { opacity: 1, transition: varTranEnter() },
+			exit: { opacity: 0, transition: varTranExit() },
 		},
 		inUp: {
 			initial: { y: distance, opacity: 0 },
